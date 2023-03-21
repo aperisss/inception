16852e060a4f4911d8e16c3ed69eae20bf9e81e8
Adam PERIS <aperis@e2r13p13.42.fr>
2023-03-21 11:18:02 +0100

    Dockerfile entrypoint

--- srcs/requirements/mariadb/tools/create_db.sh
+++ srcs/requirements/mariadb/tools/create_db.sh
@@ -1,10 +1,6 @@
 CREATE DATABASE IF NOT EXISTS wordpress;
 
 echo "Aaaaaaaaaaaaaaaaaaaaa";
-echo $DB_USER_PASSWORD;
-echo $DB_ROOT_PASSWORD;
-echo $DB_NAME;
-echo $DB_USER;
 echo "BBBBBBBBBBBBBBbbbbbb";
 CREATE USER $DB_USER@'%';
 SET PASSWORD FOR $DB_USER@'%' = PASSWORD('$DB_USER_PASSWORD');
