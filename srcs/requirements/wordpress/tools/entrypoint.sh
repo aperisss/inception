d50b6bf1752f931fdeff6c316a7d202325c4fd41
Adam PERIS <aperis@e3r13p4.clusters.42paris.fr>
2023-03-22 14:02:59 +0100

    Dockerfile entrypoint

--- srcs/requirements/wordpress/tools/entrypoint.sh
+++ srcs/requirements/wordpress/tools/entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-sed -ie 's/$DB_USER/'$DB_USER'/' /var/www/wordpress/wp-config.php # replace $DB_USER by $DB_ADMIN in wp-config.php file 
-sed -ie 's/$DB_NAME/'$DB__NAME'/' /var/www/wordpress/wp-config.php # replace $DB_NAME by $DB_DATA_BASE_NAME in wp-config.php file
-sed -ie 's/$DB_PASSWORD/'$DB_USER_PASSWORD'/' /var/www/wordpress/wp-config.php # replace $DB_PASSWORD by $DB_ADMIN_PASSWORD in wp-config.php file
+sed -ie 's/$DB_USER/'$DB_ADMIN'/' /var/www/wordpress/wp-config.php # replace $DB_USER by $DB_ADMIN in wp-config.php file 
+sed -ie 's/$DB_NAME/'$DB_DATA_BASE_NAME'/' /var/www/wordpress/wp-config.php # replace $DB_NAME by $DB_DATA_BASE_NAME in wp-config.php file
+sed -ie 's/$DB_PASSWORD/'$DB_ADMIN_PASSWORD'/' /var/www/wordpress/wp-config.php # replace $DB_PASSWORD by $DB_ADMIN_PASSWORD in wp-config.php file
 
 if [ -f "/var/www/wordpress/conf" ];then # if conf file exists 
     echo "" # do nothing 
@@ -13,10 +13,10 @@ else
 	
     wp core --allow-root download --locale=fr_FR
     
-    wp core --allow-root install --url="https://aperis.42.fr/" --title="Inception aperis" --admin_name=$WP_ADMIN_USER --admin_email=$WP_ADMIN_EMAIL --admin_password=$WP_ADMIN_PASSWORD; 
+    wp core --allow-root install --url="https://mabid.42.fr/" --title="Inception mabid" --admin_name=$WP_ADMIN_USER --admin_email=$WP_ADMIN_EMAIL --admin_password=$WP_ADMIN_PASSWORD; 
     # install wordpress
 
-    wp --allow-root user create $WP_ADMIN_USER other@gmail.com --user_pass=$WP_USER_PASSWORD
+    wp --allow-root user create $WP_USER other@gmail.com --user_pass=$WP_USER_PASSWORD
     # create other user
 
     touch conf # create conf file to know that wordpress is configured
